4d645f036bf3dfbb965ff7be5182866880a1552c
Andrew Straw <strawman@astraw.com>
2014-07-13 11:11:25 +0200

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
+
 setup(name='static_site_index',
       version='0.1.0',
       packages=['static_site_index',
                 ],
       url='https://github.com/astraw/static_site_index',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
       package_data={ 'static_site_index': ['*.js','*.html'],
                      },
       )
